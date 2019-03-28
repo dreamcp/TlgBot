9088537a310c172d6d51392734698ccb6383f02c
unknown <dream@dimaspb.ru>
2019-03-28 13:13:36 +0300

    111

--- test.py
+++ test.py
@@ -1,63 +1,24 @@
-import requests  
-from bottle import Bottle, response, request as bottle_request
+import bs4
+import parser
 
+#main variables
+TOKEN = "555555555:AAAAaaaAaaA1a1aA1AAAAAAaaAAaa4AA"
+bot = telebot.TeleBot(TOKEN)
 
-class BotHandlerMixin:  
-    BOT_URL = None
+#handlers
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с хабра')
 
-    def get_chat_id(self, data):
-        """
-        Method to extract chat id from telegram request.
-        """
-        chat_id = data['message']['chat']['id']
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот - парсер хабра.')
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    else:
+        bot.send_message(chat_id, 'Простите, я вас не понял :(')
 
-        return chat_id
-
-    def get_message(self, data):
-        """
-        Method to extract message id from telegram request.
-        """
-        message_text = data['message']['text']
-
-        return message_text
-
-    def send_message(self, prepared_data):
-        """
-        Prepared data should be json which includes at least `chat_id` and `text`
-        """       
-        message_url = self.BOT_URL + 'sendMessage'
-        requests.post(message_url, json=prepared_data)
-
-
-class TelegramBot(BotHandlerMixin, Bottle):  
-    BOT_URL = 'https://api.telegram.org/bot000000000:aaaaaaaaaaaaaaaaaaaaaaaaaa/'
-
-    def __init__(self, *args, **kwargs):
-        super(TelegramBot, self).__init__()
-        self.route('/', callback=self.post_handler, method="POST")
-
-    def change_text_message(self, text):
-        return text[::-1]
-
-    def prepare_data_for_answer(self, data):
-        message = self.get_message(data)
-        answer = self.change_text_message(message)
-        chat_id = self.get_chat_id(data)
-        json_data = {
-            "chat_id": chat_id,
-            "text": answer,
-        }
-
-        return json_data
-
-    def post_handler(self):
-        data = bottle_request.json
-        answer_data = self.prepare_data_for_answer(data)
-        self.send_message(answer_data)
-
-        return response
-
-
-if __name__ == '__main__':  
-    app = TelegramBot()
-    app.run(host='localhost', port=8080)
+bot.polling()
